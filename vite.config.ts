b24fa424fb090b96c5732d1c4be7e3f9f4f57c7f
KaszubDev <kaszubowskikrzys@gmail.com>
2025-01-01 21:20:56 +0100

    edit vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import glsl from 'vite-plugin-glsl'
+import checker from 'vite-plugin-checker'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), glsl()],
+  base: '',
+  plugins: [react(), glsl(), checker({ typescript: false })],
 })
