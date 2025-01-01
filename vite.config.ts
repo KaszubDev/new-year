4c5d8f31fabe65a2a70c542eaf61b7c471d09d80
KaszubDev <kaszubowskikrzys@gmail.com>
2025-01-01 21:24:51 +0100

    config edit

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import checker from 'vite-plugin-checker'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '',
+  base: './',
   plugins: [react(), glsl(), checker({ typescript: false })],
 })
